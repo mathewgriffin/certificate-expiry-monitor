e368661397baa692a23acfe10c13c039e5c9b800
mathewgriffin <matgriffin1@gmail.com>
2019-09-03 08:48:17 +0100

    Change DB file creation

--- docker/entrypoint.sh
+++ docker/entrypoint.sh
@@ -6,5 +6,11 @@ cd /certificate-expiry-monitor && /usr/local/bin/composer install
 echo "Starting crond"
 crond
 
+echo "Creating DB files"
+echo '{}' > /certificate-expiry-monitor/storage/pre_checks.json
+echo '{}' > /certificate-expiry-monitor/storage/checks.json
+echo '{}' > /certificate-expiry-monitor/storage/deleted_checks.json
+chown -R $wwwuser /var/www/certificate-expiry-monitor-db/*.json
+
 echo "Starting supervisor"
 /usr/bin/supervisord -n -c /etc/supervisord.conf
